3c5ea9e7bbb7a47a02ad4673aed1a5c4dd8fc950
carolyn <41408998+roliestpoliest@users.noreply.github.com>
2026-04-11 21:13:59 -0500

    Initial Commit

---
+++ Tappy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tappy
+//
+//  Created by Carolyn Heron on 4/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tappy/TappyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TappyApp.swift
+//  Tappy
+//
+//  Created by Carolyn Heron on 4/11/26.
+//
+
+import SwiftUI
+
+@main
+struct TappyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
