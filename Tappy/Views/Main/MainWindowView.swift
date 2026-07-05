d3d6c71aa9859f8412fc0c3054b451ab9f055c8e
ujwal2003 <joshiujwal2003@gmail.com>
2026-07-04 19:55:11 -0500

    feat: accessibility view displays if permission is not granted to app

--- Tappy/Views/Main/MainWindowView.swift
+++ Tappy/Views/Main/MainWindowView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct MainWindowView: View {
+    @Environment(AppStateService.self) private var appState
     @Environment(\.palette) private var palette
 
     var body: some View {
@@ -14,5 +15,8 @@ struct MainWindowView: View {
         }
         .background(palette.windowBg)
         .clipShape(RoundedRectangle(cornerRadius: Metrics.windowCornerRadius))
+        .sheet(isPresented: .constant(!appState.accessibility.isGranted)) {
+            AccessibilityPromptView()
+        }
     }
 }
